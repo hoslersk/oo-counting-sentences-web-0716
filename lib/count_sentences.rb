34153712ab0cb0270a7c131092ce55e446f771e7
Schyler Hosler <schylerhosler@gmail.com>
2016-07-28 09:05:16 -0400

    Initial commit:

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,46 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    counter = 0
+    if self.empty?
+      return counter
+    else
+      words = self.split(" ")
+      words.each do |word|
+        if word.include?(".") || word.include?("?") || word.include?("!")
+          counter = counter + 1
+        end
+      end
+      return counter
+      # counter = counter + self.count(".")
+      # counter = counter + self.count("!")
+      # counter = counter + self.count("?")
+      # return counter
+    end
 
   end
 end
